7d94457be9e762699ad133382b81e2977f294f64
张潇 <zhangxiao123@sjtu.edu.cn>
2025-05-24 22:23:25 -0500

    chore: Remove the gpu driver installation part

--- commercial-5G-latency-apps/video-analytics-ar-rtp/server_analytics/env_install.sh
+++ commercial-5G-latency-apps/video-analytics-ar-rtp/server_analytics/env_install.sh
@@ -4,18 +4,18 @@ sudo apt install -y libavcodec-dev libavformat-dev libavutil-dev libavfilter-dev
 sudo apt install -y stress-ng
 pip install numpy opencv-python ultralytics posix_ipc zmq
 
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.0-1_all.deb
-sudo add-apt-repository ppa:graphics-drivers/ppa -y
-sudo dpkg -i cuda-keyring_1.0-1_all.deb
-sudo apt update
+# wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.0-1_all.deb
+# sudo add-apt-repository ppa:graphics-drivers/ppa -y
+# sudo dpkg -i cuda-keyring_1.0-1_all.deb
+# sudo apt update
 
-sudo apt install -y nvidia-driver-560
+# sudo apt install -y nvidia-driver-560
 
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb
-sudo dpkg -i cuda-keyring_1.1-1_all.deb
-sudo apt update
-sudo apt install -y cuda-toolkit-12-6
+# wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb
+# sudo dpkg -i cuda-keyring_1.1-1_all.deb
+# sudo apt update
+# sudo apt install -y cuda-toolkit-12-6
 
-echo 'export PATH=/usr/local/cuda/bin:$PATH' >> ~/.bashrc
-echo 'export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH' >> ~/.bashrc
-source ~/.bashrc
+# echo 'export PATH=/usr/local/cuda/bin:$PATH' >> ~/.bashrc
+# echo 'export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH' >> ~/.bashrc
+# source ~/.bashrc
