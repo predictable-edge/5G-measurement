36d06a76997142bdc9abe672266a939f5e0dd571
张潇 <zhangxiao123@sjtu.edu.cn>
2025-06-11 12:29:46 -0500

    chore: Add python3-pip into env setup

--- commercial-5G-latency-apps/video-transcoding-rtp/server_transcoding/env_install.sh
+++ commercial-5G-latency-apps/video-transcoding-rtp/server_transcoding/env_install.sh
@@ -1,5 +1,5 @@
 sudo apt-get -y update
-sudo apt install -y make g++ ffmpeg pkg-config
+sudo apt install -y make g++ ffmpeg pkg-config python3-pip
 sudo apt install -y libavcodec-dev libavformat-dev libavutil-dev libavfilter-dev libswscale-dev libavdevice-dev
 sudo apt install -y stress-ng
 pip3 install psutil
